801677f2021013207ae90cc26ec3b4104dd7904b
Danjelous <daniel.brunner@live.at>
2017-05-11 11:04:16 +0200

    Add microformat dummy content

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 Little exercise in the Semantic Web Course to get in touch with Microformat - University of Applied Sciences, Upper Austria.
 
 ````
-$ node index
+$ npm start
 ````
--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("YOOO");
+'use strict';
+
+const microformat = require('microformat-node');
+const prettyjson = require('prettyjson');
+let options = {};
+
+options.html = `
+<cite class="vcard">
+ <a class="fn url" href="http://meyerweb.com/">Eric Meyer</a>
+</cite> wrote a post 
+(<cite>
+  <a href="http://meyerweb.com/eric/thoughts/2005/12/16/tax-relief/">
+   Tax Relief
+  </a>
+ </cite>) 
+about an unintentionally humorous letter he received from the
+<span class="vcard">
+ <a class="fn org url" href="http://irs.gov/">Internal Revenue Service</a>
+</span>`;
+
+microformat.get(options, (err, data) => {
+
+    if(err) return next(err);
+
+    console.log(prettyjson.render(data));
+
+});
